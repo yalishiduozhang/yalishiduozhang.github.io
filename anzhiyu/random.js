e0ba2e9fd59fb4cdb3941a8a4d7a59d65fc0183d
yalishiduozhang <3134429813@qq.com>
2025-02-09 13:04:49 +0800

    Site updated: 2025-02-09 13:04:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/20/欢迎来到亚里士AI说/","2025/01/21/向上/","2025/01/20/监督学习模型/"];function toRandomPost(){
+var posts=["2025/01/21/向上/","2025/01/20/欢迎来到亚里士AI说/","2025/01/20/监督学习模型/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
