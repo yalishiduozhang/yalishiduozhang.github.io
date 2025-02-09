9d65c45c401de93a0e435e6a70f9c12c4e2d9597
yalishiduozhang <3134429813@qq.com>
2025-02-09 19:40:56 +0800

    Site updated: 2025-02-09 19:40:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/21/向上/","2025/01/20/欢迎来到亚里士AI说/","2025/01/20/监督学习模型/","2025/01/21/CS224W解题/"];function toRandomPost(){
+var posts=["2025/01/21/向上/","2025/01/20/欢迎来到亚里士AI说/","2025/01/20/监督学习模型/","2025/02/09/CS224W解题/","2025/02/09/图/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
