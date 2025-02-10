93e67111e5b1d2db33997f8baa1de97b823f28b1
yalishiduozhang <3134429813@qq.com>
2025-02-10 20:19:01 +0800

    Site updated: 2025-02-10 20:19:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/21/向上/","2025/01/20/欢迎来到亚里士AI说/","2025/01/20/监督学习模型/","2025/02/09/CS224W解题/","2025/02/09/图机器学习介绍/","2025/02/10/图机器学习浅入门知识点梳理/"];function toRandomPost(){
+var posts=[];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
